3948fd333d0d0e99fc6d74b05ddbae99820b8aeb
Nikhil <nikhil_soni_21@outlook.com>
2022-03-10 23:51:51 +0530

    Extract Audio Features to CSV

---
+++ paths.py
@@ -0,0 +1,14 @@
+DATAPATH = "D:\\Datasets\\GTZAN\\genres"
+CSV_DATAPATH = "D:\\Datasets\\GTZAN\\gtzan_30sec.csv"
+genres = [
+    'blues',
+    'classical',
+    'country',
+    'disco',
+    'hiphop',
+    'jazz',
+    'metal',
+    'pop',
+    'reggae',
+    'rock'
+]
